3e1cc1efaf531752392a2ebda044f71859b1f199
hakareem <harithakareem72@gmail.com>
2022-09-12 12:17:25 +0100

    price display exercise

---
+++ PriceDisplay.java
@@ -0,0 +1,8 @@
+public class PriceDisplay {
+ public static void main(String[] args) {
+  String name = "Coffee machine";
+  float price = (float) 15.99;
+
+  System.out.printf("The price for the item %s is %.2f \n", name, price);
+ }
+}
